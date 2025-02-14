31f3ba1f6597a23f3c1a31392a16755b3d2d9def
ryanliupie <ryanliu314@gmail.com>
2025-02-14 14:57:04 -0500

    for and while loops

---
+++ programming_practice/practice.py
@@ -0,0 +1,26 @@
+# find the sum of numbers from 0 to 4
+
+total = 0 
+for num in range (5):
+    total = total + num 
+print(total)
+
+# the total is equal to zero initally 
+# for every number in the range of 5
+# the total is added to that range 
+# total = 0 + 0 = 0 
+# total = 0 + 1 = 1 
+# total = 1 + 2 = 3
+# total = 3 + 3 = 6
+# total = 6 + 4 = 10
+
+# Remember that 0 is always counted as a number 
+
+#--------------------------------------------------------------------------------------------------
+
+i = 0 
+while i < 5:
+    print(f" Ryan five times ({i})")
+    i = i + 1
+
+# this is like the for loop where we print ryan five times until it reaches that fifth instance (i < 5)
