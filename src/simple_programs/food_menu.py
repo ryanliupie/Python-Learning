411e3e9ebbbe449604d08030c6de6924fe8316b3
ryanliupie <ryanliu314@gmail.com>
2025-06-06 08:13:44 -0400

    food menu with a dictionary

--- src/simple_programs/food_menu.py
+++ src/simple_programs/food_menu.py
@@ -13,4 +13,17 @@ for key, value in menu.items():
     print(f"{key:8}: ${value:.2f}")
 print("--------------------")
 
-choice = input("")
+while True: 
+    food = input("select a food item (q to quit): ").lower()
+    if food == "q": 
+        break
+    elif menu.get(food) is not None: 
+        cart.append(food)
+        
+for food in cart: 
+    total += menu.get(food)
+    print(food, end=" ")
+print()
+
+print(f"The total is ${total:.2f}")
+
