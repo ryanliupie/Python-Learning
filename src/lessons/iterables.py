f9ad68fe2e6320185f615b9694727bf0e8adeaf7
ryanliupie <ryanliu314@gmail.com>
2025-07-20 21:11:12 -0400

    iterables

---
+++ src/lessons/iterables.py
@@ -0,0 +1,10 @@
+# iterables: this is a object (not OOP) or collection that can return its elements one at a time. This allows it to be iterated over a loop
+
+import time
+
+list= [1, 2, 3, 4, 5, 6, 7]
+
+for i in list:
+    time.sleep(1)
+    print(i)
+
