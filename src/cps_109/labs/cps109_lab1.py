2b083d1ed837d641e7d2451c2cf90f084d6838f6
Ryan <ryanliu314@gmail.com>
2025-09-12 08:17:14 -0400

    work in progress

--- src/cps_109/labs/cps109_lab1.py
+++ src/cps_109/labs/cps109_lab1.py
@@ -7,13 +7,13 @@ import math
 
 # Question 1.  
 
-c = float(input("Enter a temperature in Celsius: ")) # i used the "float" type since the temperature could contain decimal values 
+celsius = float(input("Enter a temperature in Celsius: ")) # i used the "float" type since the temperature could contain decimal values 
 
-f = c * (9/5) + 32
-k = c + 273.15 
+fahrenheit = celsius * (9/5) + 32
+kelvin = celsius + 273.15 
 
-print("The temperature in Fahrenheit is", f)
-print("The temperature in Kelvin is", k )
+print("The temperature in Fahrenheit:", fahrenheit)
+print("The temperature in Kelvin is:", kelvin)
 
 
 # Question 2. 
@@ -23,8 +23,23 @@ b = float(input("Enter the value of the polynomial coefficient of 'b': "))
 c = float(input("Enter the value of the polynomial coefficient of 'c': "))
 
 
+x1 = -b + (math.sqrt (b ** 2) -  (4 * a * c)) / (2 * a)  
+x2 = -b - (math.sqrt (b ** 2) -  (4 * a * c)) / (2 * a)  
 
-x2 = -b - (math.sqrt(b ** 2) - (4 * a * c) / 2 * a)
-
-
+print(x1) 
 print(x2)
+
+# Question 3. 
+
+side_a = float(input("Enter the length of the triangle for side A: "))
+side_b = float(input("Enter the length of the triangle for side B: "))
+side_c = float(input("Enter the length of the triangle for side C:  "))
+
+result = side_a + side_b + side_c - max(side_a, side_b, side_c)
+print(result)
+
+# Triangle inequality theorem 
+# --> The sum of the lengths of 2 sides must be greater than the length of the third side
+
+
+
---
+++ src/cps_109/lectures/unit_2.md
@@ -0,0 +1,7 @@
+## Unit 2 
+
+### Operation on Strings 
+
+- concatenation (adding two strings) → `"Hello"` + `"Ryan"` 
+
+- successive concatenation (repeating strings) →→→
