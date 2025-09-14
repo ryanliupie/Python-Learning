04421448ebabf21705f3ac94b334cec1ee7bd477
ryanliupie <ryanliu314@gmail.com>
2025-09-14 13:08:40 -0400

    lab 1 complete

--- src/cps_109/labs/cps109_lab1.py
+++ src/cps_109/labs/cps109_lab1.py
@@ -1,45 +1,66 @@
 # CPS 109 Lab 1 
-# Questions 1 - 5 
-# Creator: Ryan 
-# ---------------
+# Questions 1 through 5 
+# Creator: Ryan Liu
+# Professor: Lugez
 
 import math
 
 # Question 1.  
 
-celsius = float(input("Enter a temperature in Celsius: ")) # i used the "float" type since the temperature could contain decimal values 
-
-fahrenheit = celsius * (9/5) + 32
+celsius = float(input("Enter a temperature in Celsius: ")) 
+fahrenheit = celsius * (9 / 5) + 32
 kelvin = celsius + 273.15 
 
-print("The temperature in Fahrenheit:", fahrenheit)
-print("The temperature in Kelvin is:", kelvin)
+print("The temperature in Fahrenheit:", round(fahrenheit, 2))
+print("The temperature in Kelvin:", round(kelvin, 2))
 
+# Question 2.
 
-# Question 2. 
+coefficient_a = float(input("Enter the value of the polynomial coefficient 'a': "))
+coefficient_b = float(input("Enter the value of the polynomial coefficient 'b': "))
+coefficient_c = float(input("Enter the value of the polynomial coefficient 'c': "))
 
-a = float(input("Enter the value of the polynomial coefficient of 'a': "))
-b = float(input("Enter the value of the polynomial coefficient of 'b': "))
-c = float(input("Enter the value of the polynomial coefficient of 'c': "))
+discriminant = coefficient_b ** 2 - 4 * coefficient_a * coefficient_c
 
+if discriminant > 0: 
+    x1 = (-coefficient_b + math.sqrt (discriminant)) / (2 * coefficient_a)  
+    x2 = (-coefficient_b - math.sqrt (discriminant)) / (2 * coefficient_a)
+    print("There are two roots:", round(x1, 2),"and", round(x2, 2)) 
 
-x1 = -b + (math.sqrt (b ** 2) -  (4 * a * c)) / (2 * a)  
-x2 = -b - (math.sqrt (b ** 2) -  (4 * a * c)) / (2 * a)  
+elif discriminant == 0: 
+    x = -coefficient_b / (2 * coefficient_a)
+    print("There is one root:", round(x, 2)) 
 
-print(x1) 
-print(x2)
+else:
+    print("There are no real roots.")
 
 # Question 3. 
 
-side_a = float(input("Enter the length of the triangle for side A: "))
-side_b = float(input("Enter the length of the triangle for side B: "))
-side_c = float(input("Enter the length of the triangle for side C:  "))
+side_a = float(input("Enter the length of the triangle for side 'a': "))
+side_b = float(input("Enter the length of the triangle for side 'b': "))
+side_c = float(input("Enter the length of the triangle for side 'c': "))
 
+maximum_value = max(side_a, side_b, side_c)
 result = side_a + side_b + side_c - max(side_a, side_b, side_c)
-print(result)
 
-# Triangle inequality theorem 
-# --> The sum of the lengths of 2 sides must be greater than the length of the third side
+print(result > maximum_value)
+
+# Question 4. 
+
+a = float(input("Enter the side length (a) of a regular pentagon: "))
+area = 1 / 4 * (math.sqrt(5 * (5 + 2 * (math.sqrt(5))))) * (a ** 2) 
+
+print("The area of the pentagon:", round(area, 2))
+
+# Question 5. 
+
+n = int(input("Enter an integer value: "))
+golden_ratio = (math.sqrt(5) + 1) /2 
+function = ((2 + golden_ratio) / (5) * (golden_ratio ** n)) + ((3 - golden_ratio) / (5) * (golden_ratio ** - n))
+
+print("The nth Fibonacci number:", round(function, 2))
+
+
 
 
 
