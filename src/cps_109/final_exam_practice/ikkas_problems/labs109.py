17b0da90617f7d131c561c7e8d2842aaa9e5c27f
Ryan <ryanliu314@gmail.com>
2025-11-13 10:14:14 -0500

    cyclops question

--- src/cps_109/final_exam_practice/ikkas_problems/labs109.py
+++ src/cps_109/final_exam_practice/ikkas_problems/labs109.py
@@ -93,3 +93,23 @@ print(only_odd_digits(0))
 
 #---------------------------------------------------------------
 
+new_list = []
+    counter = 0 
+    positive_nums = abs(n)
+    str_n = str(positive_nums)
+    if len(str_n) % 2 == 0: 
+        return False
+    else: 
+        for char in str_n: 
+            new_list.append(char)
+        for num in str_n: 
+            if num == "0": 
+                counter+= 1 
+
+        if counter > 1: 
+            return False
+        else: 
+            mid = len(str_n) // 2
+            if str_n[mid] == "0": 
+                return True
+            return False
