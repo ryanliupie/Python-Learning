576cba073d8009688d09cfd97505b90fcdf13cfe
ryanliupie <ryanliu314@gmail.com>
2025-11-13 21:24:58 -0500

    deleted 2 practice files

--- src/lessons/p.py
+++
@@ -1,7 +0,0 @@
-def colour_trio(colours): 
-    new_list = []
-    for char in colours: 
-        new_list.append(char)
-    return new_list
-
-print(colour_trio("yrbbbbryyrybb"))
--- src/lessons/practice.py
+++
@@ -1,7 +0,0 @@
-n = "1934567"
-new_list = []
-
-for char in n: 
-    new_list.append(char)
-
-print(new_list[1])
