fd7b23d6185c79ddbc7c6ceeab39f891e8e698be
ryanliupie <ryanliu314@gmail.com>
2025-12-01 15:18:19 -0500

    changes

--- src/cps_109/final_exam_practice/ikkas_problems/practice.py
+++ src/cps_109/final_exam_practice/ikkas_problems/practice.py
@@ -1,18 +1,35 @@
 def summaryRanges(nums):
-    list1 = []
+    list1 = [] 
+
+    if not nums: 
+        return list1
+                
     start = nums[0]
-    for i in range(len(nums) - 2): 
-        result = nums[i+1] - nums[i]
-        if result != 1: 
-            start == nums[i]
-            list1.append(str(start))
-    return list1
-                
-                
+
+    for i in range(1, len(nums)): 
+        if nums[i] != nums[i-1] + 1: # we start and index 1 for the value of 3. Then we compare to the first number. 
+             if start == nums[i-1]:
+                 list1.append(str(start))
+             else: 
+                 list1.append(str(start) +"->"+str(nums[i-1])) 
+             start = nums[i]
+
+    if start == nums[-1]: 
+             list1.append(str(start))  
+    else: 
+         list1.append(str(start) +"->"+str(nums[i-1]))     
+
+    return list1  
+        
+
+                  
+                                     # so basically lets say the first value is 1. we compare it and + 1 to it. 
+                                     # so now, we do oh "3 != 2?" well this is True and so it is not consective. so we append that single number to the list
 
 
 
 
-print(summaryRanges([0, 2]))
+
+print(summaryRanges([1, 3, 4, 6]))
 
 # we will do subtraction if that subtraction is equal to one, we move onto the next number 
