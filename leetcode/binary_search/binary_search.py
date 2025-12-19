8943142e84c11976ed4c78d12e586a6e34658802
ryanliupie <ryanliu314@gmail.com>
2025-12-18 21:52:44 -0500

    binary search leetcode solution

---
+++ leetcode/binary_search/binary_search.py
@@ -0,0 +1,15 @@
+def binary_search(nums, target): 
+    l =  0
+    r = len(nums) - 1 
+
+    while (l<=r): 
+        mid = (l+r) // 2
+        if nums[mid] == target: 
+            return mid 
+        elif nums[mid] < target: 
+            l = mid + 1 
+        else: 
+            r = mid - 1 
+
+
+print(binary_search([1,2,3,4,5], 3))
