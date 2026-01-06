c21aea0b85bab3f84b5d6a25e0806dd404adca6d
ryanliupie <ryanliu314@gmail.com>
2026-01-05 20:04:54 -0500

    encode and decode string leetcode problem

---
+++ leetcode/arrays_&_hashing/encode_decode_strings.py
@@ -0,0 +1,24 @@
+def encode(strs): 
+    res = ""
+    for word in strs: 
+        res += str(len(word)) + "#" + word
+    return res
+
+print(encode(["Hello", "Ryan", "Jaideep", "Cars"]))
+
+def decode(str):
+    res = []
+    i = 0 
+
+
+    while i < len(str): 
+        j = i 
+        while str[j] != "#": 
+            j += 1
+        length = int(str[i:j])
+        res.append(str[j + 1 : j + 1 + length])
+        i = j + 1 + length
+    
+    return res
+    
+print(decode("5#Hello4#Ryan7#Jaideep4#Cars"))
--- leetcode/placeholder.py
+++ leetcode/placeholder.py
@@ -1,6 +1,6 @@
-list1 = [1,2,3,4,5,6,7]
-
-for i in range(len(list1)): 
-    print(i)
-
+l1 = ["Hello", "Ryan"]
+res = ""
+for word in l1: 
+    res += str(len(word)) + "#" + word
+print(res)
     
