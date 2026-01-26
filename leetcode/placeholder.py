ee112957e3f84f3282336d9027861d4f7fbcfb66
ryanliupie <ryanliu314@gmail.com>
2026-01-26 17:26:00 -0500

    longest consecutive leetcode solution

---
+++ leetcode/placeholder.py
@@ -0,0 +1,18 @@
+def longestConsecutive(nums): 
+    set1 = set(nums)
+    max_len = 0
+
+    for num in set1: 
+        if num - 1 not in set1:
+            curr_num = num
+            curr_len = 1 
+            while curr_num + 1 in set1:
+                curr_len += 1 
+                curr_num += 1 
+            max_len = max(max_len, curr_len)
+
+    return max_len
+
+
+
+print(longestConsecutive([0,3,2,5,4,6,1,1]))
