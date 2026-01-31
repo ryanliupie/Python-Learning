942e31cbf618c87efa7d40f27c0ebf3f98658668
ryanliupie <ryanliu314@gmail.com>
2026-01-31 11:18:09 -0500

    changes

--- string_based_questions/string_manipulation/simplify_path.py
+++ string_based_questions/string_manipulation/simplify_path.py
@@ -3,7 +3,7 @@ def simplify_path(path):
     stack = []
     for str in list1: 
         if str == "..":
-            if stack: # once we come across an element, this checks "are there elements in the list". if so, we pop the most recent 
+            if stack: # once we come across an element, this checks "are there elements already in the list". if so, we pop the most recent 
                 stack.pop() # item that was recently added gets popped off. This follows the ".." logic where we go up a directory
         
         elif str != "" and str != ".": 
