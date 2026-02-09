5aa6e54bbf89cef822ff49764ccefb581a422403
ryanliupie <ryanliu314@gmail.com>
2026-02-09 09:44:38 -0500

    changes

--- leetcode/two_pointers/container_with_the_most_water.py
+++ leetcode/two_pointers/container_with_the_most_water.py
@@ -16,4 +16,23 @@ def maxArea(heights):
     return res
     
 print(maxArea([1,7,2,5,4,7,3,6]))
+
+'''e.g.
+
+1. interation 
+
+l = 0, r = 7
+
+min(heights[l], heights[r]) = 1 
+
+(r - 1) = 7 - 0 = 7
+
+We then have that 1 x 7 = 7
+
+res = 7
+
+This loops for every iteration until we have a maximum area. 
+
+
+'''
     
